ee29266155951af25a476413866b207938f63fdc
Ismail Azdad <ismail.azdad@adventori.com>
2023-01-13 18:56:46 +0100

    add new icon for home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 import styled from "styled-components"
-import LogoImg from "../../assets/Logo.svg"
+import Title from "../../assets/Title.png"
+import Title2 from "../../assets/Title2.png"
 
 const HomeWrapper = styled.div`
   display: flex;
   justify-content: center;
   background-color: #000000;
   overflow:hidden;
+  @media  only screen and (max-width:768px ){
+        justify-content: inherit;
+  }
 `
 
 const HomerContainer = styled.div`
   position: relative;
   top:30vh;
-  left: 80vh;
+  left: 35%;
   width : 100%;
   height : 100vh;  
   @media  only screen and (max-width:768px ){
-      left: 3vh;
+      left: 0vh;
   }
 `
 const LogoContainer = styled.img`
     position:relative;
     padding-top:5vh;
+    @media  only screen and (max-width:768px ){
+      width: 45vh;
+  }
+         
 `
 function Home() {
     return (
@@ -30,12 +38,12 @@ function Home() {
             <HomerContainer id="homecontainer">
                 <div>
                     <Link to="/movies">
-                        <LogoContainer src={LogoImg}/>
+                        <LogoContainer src={Title}/>
                     </Link>
                 </div>
                 <div>
                     <Link to="/movies2">
-                        <LogoContainer src={LogoImg}/>
+                        <LogoContainer src={Title2}/>
                     </Link>
                 </div>
             </HomerContainer>
