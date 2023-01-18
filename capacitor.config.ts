c1144d347b0ff3b299567c1ff88e87bc4ff8b5a3
Ismail Azdad <ismail.azdad@adventori.com>
2023-01-18 10:49:19 +0100

    Handle icon for android device

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.moviestrailer.app',
   appName: 'Movies Trailer',
   webDir: 'build',
   bundledWebRuntime: false
