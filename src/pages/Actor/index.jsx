ddaff1a24f8489d784fb5f062346758fc0c400a5
Ismail Azdad <ismail.azdad@adventori.com>
2023-03-13 12:50:28 +0100

    NEW VERSION add actor filmography page

---
+++ src/pages/Actor/index.jsx
@@ -0,0 +1,107 @@
+import React, {useState} from "react";
+import Banner from "../../components/Banner";
+import RowBanner from "../../components/RowBanner";
+import urls from "../../utils/urls"
+import { getActorMovieInfo, getGenres, useFetch} from "../../utils/hooks";
+import styled from "styled-components";
+import {Loader} from "../../utils/style/Atoms";
+import {useMediaQuery} from "react-responsive";
+import {App} from '@capacitor/app';
+import {Dialog} from '@capacitor/dialog';
+import {useParams} from "react-router";
+import ActorBio from "../../components/ActorBio";
+
+const LoaderWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const RowBannerContainer = styled.div`
+    padding-top: 50vh;
+    position: relative;
+    @media  only screen and (max-width:768px ){
+        padding-top: 30vh;
+        height:100vh;
+    }  
+`
+
+App.addListener('backButton', ({canGoBack}) => {
+    if (!!(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement)) {
+        window.screen.orientation.lock('portrait');
+        document.exitFullscreen();
+    } else {
+        if (!canGoBack) {
+            showConfirm()
+        }
+    }
+});
+
+const showConfirm = async () => {
+    const {value} = await Dialog.confirm({
+        title: 'Confirm',
+        message: `Are you sure you'd like to quit?`,
+    });
+    if (value) {
+        App.exitApp();
+    }
+};
+
+//second version of movies page , showing poster and trailer in header
+function Actor() {
+    const {id: id,language:language} = useParams()
+    const [activeIndex, setActiveIndex] = useState(null);
+    const [isLoadingActor, actor, actorError] = useFetch(urls.findActorById.replace("{id}",id)+language,false);
+    console.log('actor',actor)
+    console.log('actor url',urls.findActorById.replace("{id}",id)+language)
+    const [isLoading, data, error] = useFetch(urls.findMoviesByActorId.replace("{id}",id)+language,true);
+    const myGenres = activeIndex ? getGenres(activeIndex?.genre_ids)?.slice(0,3).join(', '): getGenres(data?.genre_ids)?.slice(0,3)?.join(', ')
+    const {genres,adults,year,popularity,imageUrl,title,overview,myId,character} =  activeIndex ? getActorMovieInfo(activeIndex) :  getActorMovieInfo(data);
+    const isMobile = useMediaQuery({query: '(max-width: 768px)'})
+    if (error) {
+        return <span>Oups something went wrong</span>
+    }
+    return (
+        <div style={{background: 'black',color:'white'}}>
+            {isLoading ? (
+                <LoaderWrapper >
+                    <Loader />
+                </LoaderWrapper>
+            ) : (
+                <div className="fixed-top" >
+                    <Banner
+                        imageUrl={imageUrl}
+                        title={title}
+                        adults={adults}
+                        popularity={popularity}
+                        year={year}
+                        genres={myGenres}
+                        overview={overview}
+                        id={myId}
+                        isMainMenu={true}
+                        showDescription={false}
+                        isMobile={isMobile}
+                        focus={false}
+                        touchState={false}
+                        language={language}
+                        activeIndex={activeIndex}
+                        setActiveIndex={setActiveIndex}
+                        character={actor?.name +' role : '+character}
+                        showSimilar={false}
+                    />
+                </div>
+
+            )}
+            <RowBannerContainer>
+                {isLoadingActor ?
+                    <LoaderWrapper >
+                        <Loader />
+                    </LoaderWrapper>
+                :
+                    <ActorBio id={id}  imageUrl={urls.findImagesUrl + actor?.profile_path} name={actor?.name}  place_of_birth={actor?.place_of_birth} biography={actor?.biography} birthday={actor?.birthday} gender={actor?.gender} profession={actor?.known_for_department}  />
+                }
+                <RowBanner sort={true} activeIndex={activeIndex} setActiveIndex={setActiveIndex} title={actor?.name+' Filmography'} url={urls.findMoviesByActorId.replace("{id}",id)+language} isLargeRow/>
+            </RowBannerContainer>
+        </div>
+    )
+}
+export default Actor
